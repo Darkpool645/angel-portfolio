35c9862983ac6297bbf14e1997de61e3dcbf0865
Darkpool645 <angel_camargo2020@outlook.com>
2025-08-03 22:01:53 -0600

    add. header component with toggle theme configuration

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,38 @@
 import { useTheme } from "@/context/ThemeContext.jsx";
+import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     const { theme, toggleTheme } = useTheme();
 
-    return(
-        <header className={"fixed top-4 left-1/2 -translate-x-1/2 w-[95%] max-w-6xl z-50 transition-all duration-300 " +
-            "rounded-xl py-3 px-4 bg-white/70 dark:bg-[#050816]/80 backdrop-blur-sm border-[0.5px] " +
-            "border-white/10"}
-            style={{ opacity: 1, transform: "none"}}>
-            <span className={"text-xl font-bold dark:text-white"}>Ángel Dev</span>
-            <button onClick={toggleTheme} className={"px-4 py-2 rounded bg-black text-white dark:bg-white " +
-                "dark:text-black transition-all"}>
-                {theme === "dark" ? "Light": "Dark"}
-            </button>
+    const navLinks = [
+        { name: "Home", href: "/" },
+        { name: "About", href: "/about" },
+        { name: "Projects", href: "/projects" },
+        { name: "Contact", href: "/contact" },
+    ];
+
+    return (
+        <header className={"flex fixed top-4 left-1/2 -translate-x-1/2 w-[95%] max-w-6xl z-50 rounded-xl py-3 px-4 " +
+            "bg-white/20 dark:bg-[#050816]/20 backdrop-blur-sm border-[0.5px] border-white/10 justify-between " +
+            "items-center transition-all duration-300"}>
+            <div className={"flex items-center justify-center"}>
+                <Link to={"/"} className={"dark:text-white text-xl font-bold whitespace-nowrap"}>Angel Dev</Link>
+            </div>
+            <div className={"w-full md:flex md:items-center md:justify-center md:space-x-8 hidden"}>
+                {navLinks.map((link) => (
+                    <Link to={link.href} key={link.name} className={"dark:text-white"}>{link.name}</Link>
+                ))}
+            </div>
+            <div className={"flex items-center justify-center"}>
+                <button onClick={toggleTheme} className={"cursor-pointer p-2"}>
+                    {theme === "dark" ? (
+                        <MoonIcon className={"size-5 text-white"} />
+                    ) : (
+                        <SunIcon className={"size-5"} />
+                    )}
+                </button>
+            </div>
         </header>
     );
 };
--- src/screens/LandingPage.jsx
+++ src/screens/LandingPage.jsx
@@ -1,9 +1,9 @@
 const LandingPage = () => {
     return (
-        <div className={"min-h-screen flex justify-center items-center"}>
+        <div className={"min-h-screen flex flex-col gap-20 justify-center items-center"}>
             <h1>Landing Page</h1>
         </div>
-    );
+    )
 };
 
 export default LandingPage;
